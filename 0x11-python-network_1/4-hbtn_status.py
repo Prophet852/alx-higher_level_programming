007ddec4c376d07b97213c521b4c3f449ec0f8f5
Prophet852 <dingawillie@outlook.com>
2024-03-02 18:51:59 -0800

    network 1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""Fetches the URL: https://intranet.hbtn.io/status
-with `requests` module
-"""
-
+'''
+Script that fetches https://intranet.hbtn.io/status
+'''
 import requests
 
-
 if __name__ == "__main__":
-    req = requests.get('https://intranet.hbtn.io/status')
-
+    url = requests.get('https://intranet.hbtn.io/status')
     print('Body response:')
-    print('\t- type: {_type}'.format(_type=type(req.text)))
-    print('\t- content: {_content}'.format(_content=req.text))
+    print('\t- type: {}'.format(type(url.text)))
+    print('\t- content: {}'.format(url.text))
